1239fa0f85dff3ff881336bef1b8f5fdad32c3be
tehtelev <tehtelev@gmail.com>
2025-08-14 18:47:52 +0300

    2.0.1 для 1.20.12

--- ElectricalProgressive-Basics/ElectricalProgressiveBasics.cs
+++ ElectricalProgressive-Basics/ElectricalProgressiveBasics.cs
@@ -17,13 +17,13 @@ using System.Collections.Generic;
 
 
 [assembly: ModDependency("game", "1.21.0-rc.4")]
-[assembly: ModDependency("electricalprogressivecore", "2.1.0-rc.6")]
+[assembly: ModDependency("electricalprogressivecore", "2.1.0-rc.4")]
 [assembly: ModInfo(
     "Electrical Progressive: Basics",
     "electricalprogressivebasics",
     Website = "https://github.com/tehtelev/ElectricalProgressive",
     Description = "Basic electrical devices.",
-    Version = "2.1.0-rc.6",
+    Version = "2.1.0-rc.4",
     Authors = new[] {
         "Tehtelev",
         "Kotl"
