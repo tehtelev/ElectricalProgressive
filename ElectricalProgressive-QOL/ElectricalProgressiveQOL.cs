57980934b5fdea0208ab2e7843a53733e8fffa14
tehtelev <tehtelev@gmail.com>
2025-08-24 13:25:05 +0300

    Некоторые устройства могут теперь принимать энергию на все грани. 2.1.0-rc6

--- ElectricalProgressive-QOL/ElectricalProgressiveQOL.cs
+++ ElectricalProgressive-QOL/ElectricalProgressiveQOL.cs
@@ -24,7 +24,7 @@ using Vintagestory.API.Common;
     "electricalprogressiveqol",
     Website = "https://github.com/tehtelev/ElectricalProgressive",
     Description = "Additional electrical devices.",
-    Version = "2.0.2",
+    Version = "2.1.0-rc.5",
     Authors = new[] {
         "Tehtelev",
         "Kotl"
