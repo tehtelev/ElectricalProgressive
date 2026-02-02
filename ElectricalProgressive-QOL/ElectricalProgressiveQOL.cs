c704319017ba8f96bce1e875964d1164173b6d20
tehtelev <tehtelev@gmail.com>
2026-02-02 22:35:37 +0300

    - 2.6.7 QoL обновление горна для работы с желобами - Бонусом беларусская локализация
    - 2.6.7 QoL update for working with chutes
    - Belarusian localization as a bonus

--- ElectricalProgressive-QOL/ElectricalProgressiveQOL.cs
+++ ElectricalProgressive-QOL/ElectricalProgressiveQOL.cs
@@ -29,7 +29,7 @@ using Vintagestory.API.Server;
     "electricalprogressiveqol",
     Website = "https://github.com/tehtelev/ElectricalProgressive",
     Description = "Additional electrical devices.",
-    Version = "2.6.6",
+    Version = "2.6.7",
     Authors =
     [
         "Tehtelev",
