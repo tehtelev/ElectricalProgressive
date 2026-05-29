467c60b8c982df3da623245029d69a4fe722343c
Kotl-EV <74097230+Kotl-EV@users.noreply.github.com>
2026-05-29 23:16:04 +0300

    версия up

--- ElectricalProgressive-Equipment/ElectricalProgressiveEquipment.cs
+++ ElectricalProgressive-Equipment/ElectricalProgressiveEquipment.cs
@@ -17,7 +17,7 @@ using Vintagestory.API.Common.Entities;
     "electricalprogressiveequipment",
     Website = "https://github.com/tehtelev/ElectricalProgressive",
     Description = "Electric weapons, armor and tools",
-    Version = "3.1.0",
+    Version = "3.2.0",
     Authors =
     [
         "Tehtelev",
